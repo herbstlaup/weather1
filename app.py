17811003ebb5f42cf8aaebf260144564b0264014
herbstlaup <33521234+herbstlaup@users.noreply.github.com>
2023-01-30 20:36:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 
 from flask import Flask
+from flask import send_file
 app = Flask(__name__)
 
 @app.route('/')
 def Weather_Ensembles():
-    return '<h1>Hello, World ! - Pyflask Demo</h1>'
+    return '<h1>Weatner Ensemble Demo</h1>'
+
 
 @app.route('/temp1')
 def get_temp1():
-    return '<h1>App version : <b>2.0</b></h1>'
+    return send_file('forecast temp1.png', mimetype='image/png')
 
 @app.route('/temp2')
 def get_temp2():
-    return '<h1>You are accessing /test endpoint</h1>'
+    return '<h1>You are accessing /temp2 endpoint</h1>'
 
   
 @app.route('/wind1')
 def get_wind1():
-  return '<h1>You are accessing /test endpoint</h1>'
+  return '<h1>You are accessing /wind1 endpoint</h1>'
 
 @app.route('/wind2')
   def get_wind2():
-  return '<h1>You are accessing /test endpoint</h1>'
+  return '<h1>You are accessing /wind2 endpoint</h1>'
 
 
 if __name__ == "__main__":
